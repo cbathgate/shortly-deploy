1c2f251480db2ead08a76e469e749ab67a1abf66
Hack Reactor Students <accounts@hackreactor.com>
2017-01-19 11:56:27 -0800

    Retesting last commit - Added our personal github profiles to the gitpush task

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function(grunt) {
     gitpush: {
       'your_target': {
         options: {
-          remote: 'live'
+          remote: ['origin', 'live', 'anthony']
         }
       }
     },
